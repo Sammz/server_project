4c3cdb1bc7f9b22424f1a959b83666c62f92640e
sammz <sam.witt25@gmail.com>
2019-07-23 17:58:51 +0200

    Follow a tutorial a bit

--- server_project/settings.py
+++ server_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bierlijst.apps.BierlijstConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
